95a970a35b44df0099a813d0720c400bacc90b53
deoxysjr <deoxysjr@gmail.com>
2017-02-06 10:59:21 +0100

    Update random.cs

--- random.cs
+++ random.cs
@@ -1,1 +1,52 @@
+using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace SuperBot
+{
+    class MyBot
+    {
+      DiscordClient discord;
+      CommandService commands;
+      
+      public MyBot()
+      {
+          discord = new DiscordClient(x =>            
+          {                
+          x.LogLevel = LogSeverity.Info;                
+          x.LogHandler = Log;            
+          });
+          
+          discord.UsingCommands(x =>            
+          {                
+          x.PrefixChar = '%';                
+          x.AllowMentionPrefix = true;                
+          //x.HelpMode = HelpMode.Public;            
+          });
+          
+          commands = discord.GetService<CommandService>();
+          
+          discord.ExecuteAndWait(async () =>            
+          {                
+              while (true)                
+              {                    
+              await discord.Connect("token", TokenType.Bot);                    
+              Console.WriteLine("Bot connected correctly");                    
+              discord.SetGame("%help for commands");                    
+							});                    
+							break;
+							}
+					 });						
+
+			private void Log(object sender, LogMessageEventArgs e)        
+			{            
+			Console.WriteLine($"[{e.Source}] {e.Message}");
+			}
+	 }
+}
