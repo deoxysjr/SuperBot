6240fa7f5f21a9f711206908c2bec493b51cc551
deoxysjr <deoxysjr@gmail.com>
2017-03-14 12:39:20 +0100

    Update maand.cs

--- maand.cs
+++ maand.cs
@@ -1,1 +1,49 @@
+public static string Month = "januari";
 
+if(DateTime.Month == 1)
+{
+	Month = "januari";
+}
+if(DateTime.Month == 2)
+{
+	Month = "feberuari";
+}
+if(DateTime.Month == 3)
+{
+	Month = "maart";
+}
+if(DateTime.Month == 4)
+{
+	Month = "april";
+}
+if(DateTime.Month == 5)
+{
+	Month = "mei";
+}
+if(DateTime.Month == 6)
+{
+	Month = "juni";
+}
+if(DateTime.Month == 7)
+{
+	Month = "juli";
+}
+if(DateTime.Month == 8)
+{
+	Month = "augustus";
+}
+if(DateTime.Month == 9)
+{
+	Month = "septemder";
+}
+if(DateTime.Month == 10)
+{
+	Month = "oktober";
+}
+if(DateTime.Month == 11)
+{
+	Month = "november";
+}if(DateTime.Month == 12)
+{
+	Month = "december";
+}
