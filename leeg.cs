7e24ab16665babe4d9b4c9b969efba024a1605ba
deoxysjr <deoxysjr@gmail.com>
2017-06-29 16:16:13 +0200

    Update leeg.cs

--- leeg.cs
+++ leeg.cs
@@ -1,9 +1,6 @@
 using Discord;
 using Discord.Commands;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
